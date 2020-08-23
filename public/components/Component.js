39c5adab2343dad08990bc32b5e2c977157e0940
Oleksandr <skrypka.o.v@gmail.com>
2020-08-23 11:30:51 +0300

    Configured ESLint

--- public/components/Component.js
+++ public/components/Component.js
@@ -1,6 +1,7 @@
 //Component class is used for the rendering of changed DOM elements
 export default class Component {
   constructor(componentsId, store) {
+    // @ts-ignore
     this._render_ = this.render.bind(this);
     store.events.subscribe(componentsId, this._render_);
   }
